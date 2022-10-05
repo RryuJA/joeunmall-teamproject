54fd7d158e84e505904b419ccbef4b699143a086
Gyuseong Cho <rbtjd6895@naver.com>
2022-10-05 19:45:02 +0900

    GSC five commit

---
+++ joeun_html/js/user-shoppingBasket.js
@@ -0,0 +1,89 @@
+function count1(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById("result1");
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === "plus") {
+    number = parseInt(number) + 1;
+  } else if (type === "minus") {
+    number = parseInt(number) - 1;
+  }
+
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count2(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById("result2");
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === "plus") {
+    number = parseInt(number) + 1;
+  } else if (type === "minus") {
+    number = parseInt(number) - 1;
+  }
+
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count3(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById("result3");
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === "plus") {
+    number = parseInt(number) + 1;
+  } else if (type === "minus") {
+    number = parseInt(number) - 1;
+  }
+
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count4(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById("result4");
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === "plus") {
+    number = parseInt(number) + 1;
+  } else if (type === "minus") {
+    number = parseInt(number) - 1;
+  }
+
+  // 결과 출력
+  resultElement.innerText = number;
+}
+
+function count5(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById("result5");
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === "plus") {
+    number = parseInt(number) + 1;
+  } else if (type === "minus") {
+    number = parseInt(number) - 1;
+  }
+
+  // 결과 출력
+  resultElement.innerText = number;
+}
