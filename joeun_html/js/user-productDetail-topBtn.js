4f62d20cd129f8ffacf3496d74a91b509ded8397
Jaemin Park <catbirdseat@naver.com>
2022-10-11 11:06:13 +0900

    PJM 9th commit
    - top button 완료
    - 이미지 테두리 제거
    - 돋보기 기능 제거

--- joeun_html/js/user-productDetail-topBtn.js
+++ joeun_html/js/user-productDetail-topBtn.js
@@ -1,12 +1,13 @@
 $(function() {
     // 보이기 | 숨기기
     $(window).scroll(function() {
-       if ($(this).scrollTop() > 300) { //300 넘으면 버튼이 보여짐
+       if ($(this).scrollTop() > 200) { //300 넘으면 버튼이 보여짐
              $('#topButton').fadeIn();
              } else {
              $('#topButton').fadeOut();
        }
     });
+
     // 버튼 클릭시
     $("#topButton").click(function() {   
     $('html, body').animate({scrollTop : 0/*0 까지 animation 이동*/}, 100 /*속도 100*/);          
