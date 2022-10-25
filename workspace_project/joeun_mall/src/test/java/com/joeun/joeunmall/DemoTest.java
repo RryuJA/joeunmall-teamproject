5364446c1cc0d4de8031e8b9dbb08199252cf4bf
RryuJA <rja8741@gmail.com>
2022-10-25 10:00:48 +0900

    spring initial setup

---
+++ workspace_project/joeun_mall/src/test/java/com/joeun/joeunmall/DemoTest.java
@@ -0,0 +1,17 @@
+package com.joeun.joeunmall;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DemoTest {
+
+	@Test
+	public void test() {
+		log.info("demotest");
+	}
+
+}
